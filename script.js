a18d44bf25d035664aee35718b36593d18e9f60b
brksaian <braianviana@hotmail.com>
2023-10-18 14:26:16 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleTheme() {
+  var element = document.documentElement;
+  element.classList.toggle("light");
+
+  const img = element.querySelector("#profile img");
+
+  if (element.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar.png");
+  } else {
+    img.setAttribute("src", "./assets/avatar-light.png");
+  }
+}
